d8c4f30a30a5c8c959d097f9bf93c440660928d9
ShaileshLearns <shaileshlearnstech@gmail.com>
2023-04-12 17:44:04 +0530

    First Commit
    Uploaded scraper, chrome driver and the resultant output csv file.

---
+++ test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from shutil import which
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException
+import pandas as pd
+import time
+
+
+
+print ("Hello World")
+rupee = '\u20B9'
+print(rupee)
